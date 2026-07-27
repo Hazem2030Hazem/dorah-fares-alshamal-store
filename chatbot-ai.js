64b2c5a06c26bcaa97eab736864ad0b345918ae1
Hazem2030Hazem <hazemalheandawy2030@gmail.com>
2026-07-27 20:03:12 +0300

    Update chatbot-ai.js

--- chatbot-ai.js
+++ chatbot-ai.js
@@ -45,7 +45,7 @@ var doraChatbot = {
         try {
             var fullMessage = this.getSystemPrompt() + '\n\nسؤال العميل: ' + msg;
             
-            var response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=AQ.Ab8RN6IRcpbeYXSHI6G4-ZEKQZWTR3BSYBykJzgajrBXvfJcaA', {
+            var response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=AQ.Ab8RN6Ii6OuR9Tf2HPQ6bmHQe1Uvn3ox98u_WzFQtkR6WilRyw', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ contents: [{ parts: [{ text: fullMessage }] }] })
