c09d14b7bc538968a5b26333f9dc8a96ef16ba65
Hazem2030Hazem <hazemalheandawy2030@gmail.com>
2026-07-29 14:37:54 +0300

    Update admin-v2.js

--- admin-v2.js
+++ admin-v2.js
@@ -86,7 +86,7 @@ window.loadProducts=async function(){
 };
 
 window.editProduct = async function(id) {
-    var { data } = await supabaseClient.from('store_products').select('*').eq('id', id).single();
+var { data } = await supabaseClient.from('store_products').select('*').order('id', { ascending: false });
     if (!data) return;
     document.getElementById('productId').value = data.id;
     document.getElementById('productName').value = data.name;
